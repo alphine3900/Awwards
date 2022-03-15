85b2802b1bc9de9a3116c3166f42193e5d4b8402
alphine3900 <alphine.kirimi@student.moringaschool.com>
2022-03-15 23:32:49 +0300

    views file

--- rateapp/urls.py
+++ rateapp/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import url
-from . import views
+from rateapp import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 
 urlpatterns=[
-  url('^$',views.index,name = 'index'),
+   url(r'^$',views.login_view, name='login'),
+  url(r'^register/',views.register_view, name='register'),
+  url(r'^index/',views.index,name = 'index'),
   url(r'^search/', views.search_results, name='search_results'),
   url(r'^new/project$', views.new_project, name='new-project'),
   url(r'^user/(?P<username>\w+)', views.profile, name='profile'),
